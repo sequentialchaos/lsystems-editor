93f9c00094f19b89f52e0a8de5aa5d1e6a658b57
Winston Smith <wsbraves@gmail.com>
2019-11-17 22:00:15 -0500

    initialize repository

---
+++ js/sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+	createCanvas(innerWidth, innerHeight)
+	noLoop()
+}
+
+function draw() {
+	background(20)
+}
+
+function windowResized() {
+	resizeCanvas(innerWidth, innerHeight)
+}
