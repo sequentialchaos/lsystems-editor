9d018c66d1b863d7c6ebda321c019963812ed758
Winston Smith <wsbraves@gmail.com>
2019-11-25 18:57:49 -0500

    create colors.js

---
+++ js/colors.js
@@ -0,0 +1,17 @@
+function colorMap(
+	{
+		name = 'rainbow',
+		index = 0,
+		length = 0,
+		saturation = 1,
+		brightness = 1,
+		alpha = 1,
+		colors = [],
+		cycles = 10
+	} = {}
+) {
+	if (name == 'rainbow') {
+		let hue = (index * cycles / length) % 1
+		stroke(hue, saturation, brightness, alpha)
+	}
+}
